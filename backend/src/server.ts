de35afc814ea2acf29c4db133237c368f3a8ffe2
Michael Nascimento <michael.dev.nascimento@gmail.com>
2024-07-16 17:56:53 -0300

    feat: router about create/get/update trip, invite and participants

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,13 +8,14 @@ import { createActivity } from './routes/create-activity';
 import { getActivities } from './routes/get-activities';
 import { createLink } from './routes/create-link';
 import { getLinks } from './routes/get-links';
+import { getParticipants } from './routes/get-participants';
+import { createInvite } from './routes/create-invite';
+import { updateTrip } from './routes/update-trip';
+import { getTripDetails } from './routes/get-trip-details';
+import { getParticipantDetails } from './routes/get-participant-details';
 
 const app = fastify()
-
-
-app.register(cors, {
-    origin: '*'
-})
+app.register(cors, { origin: '*' })
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler)
@@ -26,6 +27,11 @@ app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipantDetails)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running!');
