25a581286c45f3819b7881624af093e5c691a918
Michael Nascimento <michael.dev.nascimento@gmail.com>
2024-07-16 19:26:02 -0300

    feat: env

--- backend/src/routes/confirm-participant.ts
+++ backend/src/routes/confirm-participant.ts
@@ -3,6 +3,7 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod';
 import { prisma } from "../lib/prisma";
 import { ClientError } from "../errors/client-error";
+import { env } from "../env";
 
 export async function confirmParticipant(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get('/participants/:participantId/confirm', {
@@ -26,7 +27,7 @@ export async function confirmParticipant(app: FastifyInstance) {
     }
 
     if (participant.is_confirmed) {
-      return reply.redirect(`http://localhost:3000/trips/${participant.trip_Id}`)
+      return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_Id}`)
     }
 
     await prisma.participant.update({
@@ -34,6 +35,6 @@ export async function confirmParticipant(app: FastifyInstance) {
       data: { is_confirmed: true }
     })
 
-    return reply.redirect(`http://localhost:3000/trips/${participant.trip_Id}`)
+    return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_Id}`)
   })
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,6 +14,7 @@ import { updateTrip } from './routes/update-trip';
 import { getTripDetails } from './routes/get-trip-details';
 import { getParticipantDetails } from './routes/get-participant-details';
 import { errorHandler } from './error-handler';
+import { env } from './env';
 
 const app = fastify()
 app.register(cors, { origin: '*' })
@@ -36,6 +37,6 @@ app.register(updateTrip)
 app.register(getTripDetails)
 app.register(getParticipantDetails)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
     console.log('Server running!');
 })
