272dee4d433213fcb5a6a10187adc8ebc3e1d287
Michael Nascimento <michael.dev.nascimento@gmail.com>
2024-08-19 12:00:07 -0300

    feat: fim das aulas

--- frontend/src/pages/create-trip/index.tsx
+++ frontend/src/pages/create-trip/index.tsx
@@ -4,6 +4,8 @@ import { InviteGuestsModal } from "./invite-guests-modal";
 import { ConfirmTripModal } from "./confirm-trip-modal";
 import { DestinationAndDateStep } from "./steps/destination-and-data-step";
 import { InviteGuestStep } from "./steps/invite-guests-step";
+import { DateRange } from "react-day-picker";
+import { api } from "../../lib/axios";
 
 export function CreateTripPage() {
   const navigate = useNavigate();
@@ -12,9 +14,16 @@ export function CreateTripPage() {
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
   const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false);
 
+  const [destination, setDestination] = useState("");
+  const [ownerName, setOwnerName] = useState("");
+  const [ownerEmail, setOwnerEmail] = useState("");
+  const [eventStartAndEndDates, setEventStartAndEndDates] = useState<
+    DateRange | undefined
+  >();
+
   const [emailsToInvite, setEmailsToInvite] = useState([
     "michael.dev.nascimento@gmail.com",
-    "michael.teste@gmail.com",
+    "teste.teste@gmail.com",
   ]);
 
   function openGuestsInput() {
@@ -63,10 +72,29 @@ export function CreateTripPage() {
     setEmailsToInvite(newEmailList);
   }
 
-  function createTrip(event: FormEvent<HTMLFormElement>) {
+  async function createTrip(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    navigate("/trips/123");
+    if (!destination) return;
+
+    if (!eventStartAndEndDates?.from || !eventStartAndEndDates?.to) return;
+
+    if (emailsToInvite.length === 0) return;
+
+    if (!ownerName || !ownerEmail) return;
+
+    const response = await api.post("/trips", {
+      destination,
+      starts_at: eventStartAndEndDates.from,
+      ends_at: eventStartAndEndDates.to,
+      owner_name: ownerName,
+      owner_email: ownerEmail,
+      emails_to_invite: emailsToInvite,
+    });
+
+    const { tripId } = response.data;
+
+    navigate(`/trips/${tripId}`);
   }
 
   return (
@@ -83,6 +111,9 @@ export function CreateTripPage() {
             closeGuestsInput={closeGuestsInput}
             isGuestsInputOpen={isGuestsInputOpen}
             openGuestsInput={openGuestsInput}
+            setDestination={setDestination}
+            eventStartAndEndDates={eventStartAndEndDates}
+            setEventStartAndEndDates={setEventStartAndEndDates}
           />
 
           {isGuestsInputOpen && (
@@ -120,6 +151,8 @@ export function CreateTripPage() {
         <ConfirmTripModal
           closeConfirmTripModal={closeConfirmTripModal}
           createTrip={createTrip}
+          setOwnerName={setOwnerName}
+          setOwnerEmail={setOwnerEmail}
         />
       )}
     </div>
--- frontend/src/pages/trip-details/activities.tsx
+++ frontend/src/pages/trip-details/activities.tsx
@@ -1,9 +1,67 @@
 import { CircleCheck } from "lucide-react";
+import { api } from "../../lib/axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+interface Activity {
+  date: string;
+  activities: {
+    id: string;
+    title: string;
+    occurs_at: string;
+  }[];
+}
 
 export function Activities() {
+  const { tripId } = useParams();
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/activities`)
+      .then((response) => setActivities(response.data.activities));
+  }, [tripId]);
+
   return (
     <div className="space-y-8">
-      <div className="space-y-2.5">
+      {activities.map((activityOnDay) => {
+        return (
+          <div className="space-y-2.5" key={activityOnDay.date}>
+            <div className="flex gap-2 items-baseline">
+              <span className="text-xl text-zinc-300 font-semibold">
+                Dia {format(activityOnDay.date, "dd")}
+              </span>
+              <span className="text-xs text-zinc-500 capitalize">
+                {format(activityOnDay.date, "EEEE", { locale: ptBR })}
+              </span>
+            </div>
+            {activityOnDay.activities.length > 0 ? (
+              <div className="">
+                {activityOnDay.activities.map((activity) => {
+                  return (
+                    <div className="space-y-2.5" key={activity.id}>
+                      <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{activity.title}</span>
+                        <span className="text-zinc-400 text-sm ml-auto">
+                          {format(activity.occurs_at, "p", { locale: ptBR })}
+                        </span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              <p className="text-zinc-500 text-sm">
+                Nenhuma atividade cadastrada nesta data.
+              </p>
+            )}
+          </div>
+        );
+      })}
+      {/* <div className="space-y-2.5">
         <div className="flex gap-2 items-baseline">
           <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
           <span className="text-xs text-zinc-500">Sábado</span>
@@ -32,7 +90,7 @@ export function Activities() {
             <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
